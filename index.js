51ce8474b5a3547e4d9ca9575266ccd232d51c0a
FaizanRaza144 <fivacraza@gmail.com>
2023-09-24 19:17:57 +0500

    chnages in index js

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const DBConnect = require('./Database/database');
 const route = require('./Routes/routes');
 const errorHandler = require('./middleware/errorHandler');
 const cors = require('cors');
+const memberController= require('./controllers/memberController');
+const plotController = require('./controllers/plotController');
+const accounts = require('./controllers/accounts');
+const ledger = require('./controllers/ledger');
+const adminController = require('./controllers/adminController');
 
 app.use(cors(
     {
@@ -19,6 +24,63 @@ app.get("/", (req, res) => {
     res.json("Hello");
 })
 
+//Admin Routes
+route.post('/admin',adminController.login);
+route.put('/admin/passwordReset/:id',adminController.passwordReset);
+route.post('/admin/permissions',adminController.permissions);
+route.post('/admin/memberRegister',adminController.registerMember);
+route.get('/admin/getAllUsers',adminController.getAllUser);
+route.post('/admin/role',adminController.role);
+route.post('/admin/Register',adminController.Register);
+route.post('/admin/logout',adminController.logout);
+//MEMBERS MANAGEMENT******************************************************
+//Members login Route
+route.post('/members',memberController.login);
+
+//Members Register Route
+route.post('/members/membersRegister',memberController.register);
+
+//View ALL members
+route.get('/members/all',memberController.all);
+
+//View member by id
+route.get('/members/:id',memberController.getById);
+
+//Edit member by id
+route.put('members/:id',memberController.update);
+
+//Delete member by id
+route.delete('/members/:id', memberController.delete);
+
+//PLOTS**************************************************************
+//Add Plots
+route.get('/plots',plotController.add);
+
+//Add pLot
+route.post('/plots/all',plotController.all);
+
+//View Plots By ID
+route.get('/plots/:id',plotController.getById);
+
+//Delete Plots By ID
+route.delete('/plots/:id',plotController.delete);
+
+//Update Plot
+route.put('/plots/:id',plotController.update);
+
+//Accounts*****************************************************************
+route.post('/accounts',accounts.add);
+route.get('/accounts/all',accounts.all);
+route.get('/getAccountById/:id',accounts.getById);
+route.put('/accounts/:id',accounts.update);
+route.delete('/account/:id',accounts.delete);
+
+//LEDGERS**********************************************************************
+route.post('/ledger',ledger.add);
+route.get('/ledger/all',ledger.all);
+route.get('/ledger/:id',ledger.getById);
+route.put('/ledger/:id',ledger.update);
+route.delete('/ledger/:id',ledger.delete);
 DBConnect();
 
 app.use(express.json());
